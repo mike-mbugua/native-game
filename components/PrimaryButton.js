0a58f30a31e8baff886f070d24e710aff4cd99c7
michael mbugua <mikembugua960@gmail.com>
2024-01-22 09:38:53 +0300

    create custom button

---
+++ components/PrimaryButton.js
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet, Pressable } from "react-native";
+
+function PrimaryButton({ children }) {
+  const pressHandler = () => {
+    console.log("Pressed");
+  };
+  return (
+    <Pressable onPress={pressHandler}>
+      <View>
+        <Text style={styles.button}>{children}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default PrimaryButton;
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderColor: "#ddb52f",
+    fontSize: 15,
+    color: "white",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+    textAlign: "center",
+    margin: 5,
+    // border: "#9AB5E7",
+    // borderWidth: 2,
+  },
+});
