29f635300c9418b863a8e24abd4f339e281702d7
michael mbugua <mikembugua960@gmail.com>
2024-01-23 06:57:39 +0300

    create game screen component

---
+++ screens/GameScreen.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet } from "react-native";
+
+function GameScreen() {
+  return <Text style={styles.screenHandler}>Game screen</Text>;
+}
+
+export default GameScreen;
+const styles = StyleSheet.create({
+  screenHandler: {
+    marginTop: 20,
+  },
+});
