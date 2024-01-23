68b4fe392414995a399f61a2b804e3e6a0da1665
michael mbugua <mikembugua960@gmail.com>
2024-01-23 13:53:54 +0300

    generate random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomNumbersBetween(min, max, exclude) {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumber === exclude) {
+    return generateRandomNumbersBetween(max, min, exclude);
+  } else {
+    return randomNumber;
+  }
+}
+function GameScreen({ userNumber }) {
+  const initiaState = generateRandomNumbersBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initiaState);
   return (
     <View style={styles.screenHandler}>
       <Title>Opponent's Guess</Title>
-      {/* <Text>Mike</Text> */}
+      <NumberContainer>{currentGuess}</NumberContainer>
     </View>
   );
 }
